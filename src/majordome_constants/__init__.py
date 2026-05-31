c98db9213aee4c82b0a63dc2f68c83b8ec7f2c83
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-05-31 11:41:30 +0200

    Standard design

--- src/majordome_constants/__init__.py
+++ src/majordome_constants/__init__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from typing import Any
-from .constants import *
+from . import constants as ext
 
 
-__all__ = sorted([x for x in dir(constants) if not x.startswith("_")])
+__all__ = sorted([x for x in dir(ext) if not x.startswith("_")])
 
 
 def __getattr__(name: str) -> Any:
-    if name in constants:
-        return constants[name]
-    raise ImportError(f"No module named {name}")
+    try:
+        return getattr(ext, name)
+    except AttributeError:
+        raise AttributeError(f"Name '{name}' not found in scope.")
 
 
 def __dir__() -> list[str]:
